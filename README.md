16cf4033654b5cecea5ab00871dfe05094844711
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-03 13:24:38 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+## Notes app
+
+Build a notes app as a front-end, single page web app
--- note-controller-tests.js
+++ note-controller-tests.js
@@ -4,10 +4,6 @@ function NoteControllerTest() {
   list.storeNote("testing controller")
   var double = { innerHTML: null }
   double.innerHTML = new NoteListView(list).show()
-  console.log(double.innerHTML);
-
-  console.log(controller.getHtml())
-  console.log(document.getElementById('note').innerHTML)
   assert.isTrue(double.innerHTML === `<ul><li><div><a href='#notes/0'>testing controller</a></div></li></ul`, "Notecontroller tested ")
 };
 
